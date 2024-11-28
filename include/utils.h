b2d72e3f2535d48e62e2eb59aa538b1e5af11cb4
Paul-Englert <paolomini1998@gmail.com>
2024-11-28 18:07:52 +0100

    added utils.c

---
+++ include/utils.h
@@ -0,0 +1,41 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+//collection of utility functions and structs
+
+//returns the length of this string, including the nullbyte
+//if the string is mallced it must be free'd by the caller
+int StringLength(char *str);
+
+//typdef for a byte type
+typedef char byte;
+
+typedef struct {
+    void *source;
+    int offset;
+} Memory;
+
+Memory Memory_From(void *src);
+
+//reads a string from this memory and advanced the offset
+//string is malloced and has to be free'd by caller
+//if there is no null terminated string at the current position UB occurs
+const char *ReadString(Memory *memory);
+//reads an int from this memory and advances the offset
+int ReadInt(Memory *memory);
+//reads a float from this memory and advances the offset
+float ReadFloat(Memory *memory);
+//reads a byte from this memory and advances the offset
+byte ReadByte(Memory *memory);
+
+//writes a string to this memory and advances the offset
+//if the string is malloced it must be free'd by the caller
+void WriteString(Memory *memory, char *str);
+//writes an int to this memory and advances the offset
+void WriteInt(Memory *memory, int i);
+//writes a float to this memory and advances the offset
+void WriteFloat(Memory *memory, float f);
+//writes a byte to this memory and advances the offset
+void WriteByte(Memory *memory, byte b);
+
+#endif // _UTILS_H_
---
+++ src/utils.c
@@ -0,0 +1,66 @@
+#include "../include/utils.c"
+
+int StringLength(char *str) {
+    int count = 0;
+    while(str[count]) ++count;
+    return count+1;
+}
+
+Memory Memory_From(void *src) {
+    return (Memory) {src, 0};
+}
+
+const char *ReadString(Memory *memory) {
+    byte *bytes = memory->src;
+    char *str_src = &bytes[memory->offset];
+    int len = StringLength(str_src);
+    char *ret_str = malloc(len);
+    for (int i = 0; i < len; ++i) {
+        ret_str[i] = str_src[i];
+    }
+    memory->offset += len;
+    return ret_str;
+}
+int ReadInt(Memory *memory) {
+    byte *bytes = memory->src;
+    int *ptr = &bytes[memory->offset];
+    memory->offset += sizeof(int);
+    return *ptr;
+}
+float ReadFloat(Memory *memory) {
+    byte *bytes = memory->src;
+    float *ptr = &bytes[memory->offset];
+    memory->offset += sizeof(float);
+    return *ptr;
+}
+byte ReadByte(Memory *memory) {
+    byte *bytes = memory->src;
+    byte b = bytes[memory->offset];
+    ++memory->offset;
+    return b;
+}
+
+void WriteString(Memory *memory, char *str) {
+    int index = 0;
+    while (str[index] != '\0') {
+        WriteByte(memory, str[index]);
+    }
+    WriteByte(memory, '\0');
+}
+void WriteInt(Memory *memory, int i) {
+    byte *bytes = memory->src;
+    int *ptr = &bytes[memory->offset];
+    *ptr = i;
+    memory->offset += sizeof(int);
+}
+void WriteFloat(Memory *memory, float f) {
+    byte *bytes = memory->src;
+    float *ptr = &bytes[memory->offset];
+    *ptr = f;
+    memory->offset += sizeof(float);
+}
+void WriteByte(Memory *memory, byte b) {
+    byte *bytes = memory->src;
+    bytes[memory->offset] = b;
+    ++memory->offset;
+}
